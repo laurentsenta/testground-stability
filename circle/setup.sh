947f97864729c81de501b8db0489bb05537b95c0
Laurent Senta <laurent@singulargarden.com>
2022-05-12 14:49:20 +0200

    circle: background + sleep

--- circle/setup.sh
+++ circle/setup.sh
@@ -5,5 +5,5 @@ set -e
 docker cp `docker create lsenta/testground:edge`:/testground ~/bin/testground
 docker pull iptestground/sidecar:edge
 docker pull iptestground/sync-service:latest
-testground daemon | tee ~/daemon.out &
-testground healthcheck --runner local:docker --fix
+testground daemon | tee ~/daemon.out
+# testground healthcheck --runner local:docker --fix
